5700ae0105042845e5b8a0c849e104de9c05977e
OdranPrigent <odran.prigent@hotmail.fr>
2024-07-26 11:34:53 +0200

    correction marge collapse page a propos

--- src/pages/Apropos/index.jsx
+++ src/pages/Apropos/index.jsx
@@ -15,22 +15,22 @@ function Apropos (){
     <div className ="aproposC">
       <Collapse
       title="Fiabilité"
-      text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements et toutes les informations sont régulièrement vérifiées pas nos équipes."
+      text={<p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements et toutes les informations sont régulièrement vérifiées pas nos équipes.</p>}
       largeur="100%"
       />
       <Collapse
       title="Respect"
-      text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+      text={<p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>}
       largeur="100%"
       />
       <Collapse
       title="Service"
-      text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+      text={<p>Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.</p>}
       largeur="100%"
       />
       <Collapse
       title="Sécurité"
-      text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+      text={<p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>}
       largeur="100%"
       />
     </div>
